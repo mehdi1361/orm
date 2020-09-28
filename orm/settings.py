aaceba4c560ab879516c53ec9cc459b5897b1dc0
m.mousavi <mhd.mosavi@gmail.com>
2020-09-28 23:52:54 +0330

    update flake and settings.py

--- orm/settings.py
+++ orm/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for orm project.
 
@@ -54,8 +55,7 @@ ROOT_URLCONF = 'orm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

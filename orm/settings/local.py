9e7e24ca967c0e595c74f993f1ccfebd86ef3a01
m.mousavi <m.mousavi@hpds.ir>
2020-09-29 11:02:00 +0330

    update db check_db.py

--- orm/settings/local.py
+++ orm/settings/local.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'orm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test_orm_db',
+        'USER': 'postgres',
+        'PASSWORD': '<<YOUR_PASSWORD>>',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'CONN_MAX_AGE': 60,
     }
 }
 
---
+++ tests/check_db.py
@@ -0,0 +1,21 @@
+import psycopg2
+from django.conf import settings
+
+
+def test_db_exists():
+    connection = psycopg2.connect(
+        f"user='{settings.DATABASES['default']['USER']}' "
+        f"host='{settings.DATABASES['default']['HOST']}' password='{settings.DATABASES['default']['PASSWORD']}' "
+        f"port={settings.DATABASES['default']['PORT']}'"
+    )
+
+    if connection is not None:
+        connection.autocommit = True
+
+        cur = connection.cursor()
+
+        cur.execute("SELECT datname FROM pg_database;")
+
+        list_database = cur.fetchall()
+
+        assert (settings.DATABASES['default']['NAME'], ) in list_database

c55b09e326da849abf980a4ef642576127caeadc
m.mousavi <m.mousavi@hpds.ir>
2020-09-29 09:47:02 +0330

    update project

--- tests/check_apps.py
+++ tests/check_apps.py
@@ -2,9 +2,9 @@ import importlib
 from django.conf import settings
 
 
-def test_blog_module_exists():
+def test_base_module_exists():
     assert importlib.util.find_spec("base") is not None
 
 
-def test_blog_on_install_apps():
+def test_base_on_install_apps():
     assert "base.apps.BaseConfig" in settings.INSTALLED_APPS

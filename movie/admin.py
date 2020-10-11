ab922965d6630a25ca516751ef217fc294cfd783
mohsen <mirmohsenmosavi1367@gmail.com>
2020-10-11 11:46:49 +0330

    the admin is added

--- movie/admin.py
+++ movie/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from movie.models import Film
+from movie.models import Film, Actor
 
 @admin.register(Film)
 class FilmAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'release_year', 'language', 'last_update')
+    list_filter = ('last_update', )
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name',)
     list_filter = ('last_update', )

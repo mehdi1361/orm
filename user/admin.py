fb3d85e66e2d8530313463baa548b173efb2eb3e
mohsen <mirmohsenmosavi1367@gmail.com>
2020-10-12 13:41:50 +0330

    address is create

--- user/admin.py
+++ user/admin.py
@@ -1,1 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
+from user.models import Address
+
+@admin.register(Address)
+class AdderessAdmin(admin.ModelAdmin):
+    list_display = ('address', 'district', 'postal_code')
+    list_filter = ('address', )
--- user/models.py
+++ user/models.py
@@ -1,1 +1,17 @@
-# from django.db import models
+from django.db import models
+from base.models import Base, City
+from django.contrib.auth.models import User
+
+class Address(Base):
+    address = models.CharField(max_length=150)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    district = models.CharField(max_length=100)
+    postal_code = models.IntegerField()
+
+    class Meta:
+        db_table = "user_address"
+        ordering = ["address"]
+
+    def __str__(self):
+        return self.address

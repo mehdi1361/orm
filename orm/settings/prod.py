5e29f1a9d93d3c2fce2c81c2ebbd14288c2adba3
mohsen <mirmohsenmosavi1367@gmail.com>
2020-10-19 22:34:11 +0330

    servisam kard

--- orm/settings/prod.py
+++ orm/settings/prod.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig',
     'movie.apps.MovieConfig',
     'user.apps.UserConfig',
-    'rest_framework'
-    'api_v1.apps.Api_v1Config',
+    'rest_framework',
+    'api_v1.apps.ApiV1Config',
 
 ]
 

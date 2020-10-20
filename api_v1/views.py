fbf86ad0f25f63ade1e668bea8176f378d8196d7
mohsen <mirmohsenmosavi1367@gmail.com>
2020-10-20 21:51:01 +0330

    mm

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 @api_view()
 def get_date(request):
     return Response({'massege': datetime.now()})

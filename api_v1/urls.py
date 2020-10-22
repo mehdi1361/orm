535cf34e258688d1b6d9bf673840ee9a7bb44f26
mohsen <mirmohsenmosavi1367@gmail.com>
2020-10-22 18:45:57 +0330

    modify

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -3,5 +3,5 @@ from api_v1 import views
 
 urlpatterns = [
     path('api/v1/date', views.get_date),
-    path('api/v1/base/country{site_url}', views.list_country),
+    path('api/v1/base/country', views.list_country),
 ]

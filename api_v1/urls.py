456c9224dd2ff539f26b80c3998fc9b77365f0d1
mohsen <mirmohsenmosavi1367@gmail.com>
2020-10-22 18:40:22 +0330

    ccountryserializer

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from api_v1 import views
 
 urlpatterns = [
-    path('api/v1/date', views.get_date)
+    path('api/v1/date', views.get_date),
+    path('api/v1/base/country{site_url}', views.list_country),
 ]

78826a4053d777d52c5e663c903b7a25248f80d0
mirmohsen1367 <69748055+mirmohsen1367@users.noreply.github.com>
2020-10-23 12:38:05 +0330

    Revert "a"

--- api_v1/views.py
+++ api_v1/views.py
@@ -5,7 +5,7 @@ from .seriailizers.base_serializer import DateSerializer
 from rest_framework import status
 from base.models import Country
 from api_v1.seriailizers.base_serializer import CountrySerializer
-
+from django.http import HttpResponse
 
 @api_view()
 def get_date(request):
@@ -20,10 +20,11 @@ def list_country(request):
     return Response(s.data, status=status.HTTP_200_OK,)
 
 @api_view()
+
 def get_country(request, pk):
     try:
         c = Country.objects.get(pk=pk)
         s = CountrySerializer(c)
         return Response(s.data, status=status.HTTP_200_OK,)
     except:
-        return Response(status=404)
+        HttpResponse(status=404)

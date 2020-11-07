7a59d462822a5270159778f68b7897deb6a67c89
mohsen <mirmohsenmosavi1367@gmail.com>
2020-11-07 21:54:50 +0330

    csvreader is ready

--- movie/models.py
+++ movie/models.py
@@ -3,15 +3,30 @@ from base.models import Base, Language, Category
 from django.utils import timezone
 from django.db import connection
 
+
+class Director(Base):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        db_table = 'movie_director'
+
+    def __str__(self):
+        return self.name
+
 class Film(Base):
-    RATING_CHOICES = [('R', 'R'), ('PG-13', 'PG-13'), ('NC-17', 'NC-17')]
-    title = models.CharField(max_length=30, db_index=True)
+    RATING_CHOICES = [
+        ('R', 'R'),
+        ('PG-13', 'PG-13'),
+        ('NC-17', 'NC-17')
+    ]
+
+    title = models.CharField(max_length=100, db_index=True)
     description = models.TextField()
     release_year = models.SmallIntegerField()
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
     rental_duration = models.SmallIntegerField(default=3)
     rental_rate = models.DecimalField(max_digits=4, decimal_places=2, default=4.99)
-    length = models.SmallIntegerField()
+    length = models.SmallIntegerField(null=True)
     replacement_cost = models.DecimalField(max_digits=5, decimal_places=2, default=19.99)
     rating = models.CharField(max_length=5, choices=RATING_CHOICES, default='R')
     last_update = models.DateTimeField()
@@ -19,10 +34,11 @@ class Film(Base):
     category = models.ManyToManyField(Category, through="FilmCategory")
     fa_title = models.CharField(max_length=100, null=True)
     fa_description = models.TextField(null=True)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, null=True)
 
     def save(self, *args, **kwargs):
         self.last_update = timezone.now()
-        self.language = 1
+        self.language = Language.objects.get(pk=1)
         super(Film, self).save(*args, **kwargs)
 
     class Meta:
@@ -39,17 +55,20 @@ class Film(Base):
 
 
 class Actor(Base):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     last_update = models.DateTimeField()
     film = models.ManyToManyField(Film, through="FilmActor")
+
+    def save(self, *args, **kwargs):
+        self.last_update = timezone.now()
+        super(Actor, self).save(*args, **kwargs)
 
     class Meta:
         db_table = "movie_actor"
         ordering = ["last_update"]
 
     def __str__(self):
-        return f"{self.first_name}{self.last_name}"
+        return f"{self.name}"
 
     @classmethod
     def truncate(cls):
@@ -60,6 +79,10 @@ class FilmActor(models.Model):
     film = models.ForeignKey(Film, on_delete=models.CASCADE)
     actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
     last_update = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        self.last_update = timezone.now()
+        super(FilmActor, self).save(*args, **kwargs)
 
     class Meta:
         db_table = "movie_film_actor"

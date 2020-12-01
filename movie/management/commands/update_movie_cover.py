bf968206eb53dd0f280e88f33038213977b1d5e6
mohsen <mirmohsenmosavi1367@gmail.com>
2020-12-02 03:13:44 +0330

    imagefilm

--- movie/management/commands/update_movie_cover.py
+++ movie/management/commands/update_movie_cover.py
@@ -10,35 +10,10 @@ class Command(BaseCommand):
     help = 'update movie cover'
 
     def handle(self, *args, **options):
-        # i = Image("bing", {"query": "Rogue One", "limit": 1}).find_images_link()
-        # d = i[0]
-        # d = quote(d, safe='/:?=&')
-        # s = Film.objects.get(title='Rogue One')
-        # result = urllib.request.urlretrieve(d)
-        # s.cover.save(os.path.basename(d[0]), File(open(result[0], 'rb')))
 
-
-        for  i in Film.objects.all()[:5]:
+        for i in Film.objects.all()[:5]:
             b = Image("bing", {"query": i.title, "limit": 1}).find_images_link()
             d = b[0]
-            d=quote(d, safe='/:?=&')
+            d = quote(d, safe='/:?=&')
             result = urllib.request.urlretrieve(d)
             i.cover.save(os.path.basename(d[0]), File(open(result[0], 'rb')))
-
-
-
-
-
-        # s.cover.save()
-
-
-c = Command()
-c.handle()
-
-
-
-
-
-
-
-

4c6a71b5e2d63d87691566b61cdbed1f87b69556
mohsen <mirmohsenmosavi1367@gmail.com>
2020-12-17 01:51:16 +0330

    image_url

--- orm/settings/prod.py
+++ orm/settings/prod.py
@@ -139,3 +139,5 @@ STATICFILES_DIRS = [
 #     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 #     'PAGE_SIZE': 10,
 # }
+MEDIA_URL = '/127.0.0.1:8000/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '127.0.0.1:8000/')

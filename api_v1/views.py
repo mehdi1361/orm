cc4570a9b6edee6d1a7787ef1623d46847371021
mohsen <mirmohsenmosavi1367@gmail.com>
2021-02-10 01:20:56 +0330

    modify

--- api_v1/views.py
+++ api_v1/views.py
@@ -156,7 +156,10 @@ def log_in(request):
 @api_view()
 @cache(cache_time=1 * 3600 * 24)
 def get_top_category(request, category, top):
+
     query_set = Film.objects.filter(category__name=category).order_by('-rental_rate')[0:top]
-    # q = query_set.objects.order_by('-rental_rate')[0:top]
-    s = FilmSerializer(query_set, many=True)
-    return Response(s.data, status=status.HTTP_200_OK)
+    if query_set:
+        s = FilmSerializer(query_set, many=True)
+        return Response(s.data, status=status.HTTP_200_OK)
+    else:
+        return Response(status=404)

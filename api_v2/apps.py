2406bb348d2924999d5a8275e02dfceefddaad60
mohsen <mirmohsenmosavi1367@gmail.com>
2021-03-15 01:00:17 +0330

    api_v2

---
+++ api_v2/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApiV2Config(AppConfig):
+    name = 'api_v2'
--- orm/settings/prod.py
+++ orm/settings/prod.py
@@ -50,8 +50,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+
     ),
 }
 
